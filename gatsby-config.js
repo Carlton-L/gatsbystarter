bbcf1db7834356360a8acb7d050c1a5c0ca40d85
Carlton Lindsay <carlton.l@outlook.com>
2021-10-27 18:32:00 -0700

    Create gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://SITE_URL.com/',
+    title: 'SITE_TITLE',
+    description: 'SITE_DESCRIPTION',
+    image: 'SITE_IMAGE',
+    author: 'Carlton Lindsay'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components'
+  ],
+  pathPrefix: ''
+}
